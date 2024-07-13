97f4c6e07adf7e48c69e8202f00b5cf2b33f4fbe
dhiraj2707 <71545782+dhiraj2707@users.noreply.github.com>
2024-07-13 15:17:53 +0530

    Update main.tf

--- bhau/main.tf
+++ bhau/main.tf
@@ -1,1 +1,38 @@
+provider "google" {
+  project = "test-1-429208"
+  region  = "us-west4"
+}
 
+# Create VPC network
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-vpc"
+  auto_create_subnetworks = false
+}
+
+# Create subnets within the VPC
+resource "google_compute_subnetwork" "subnet" {
+  count        = length(var.subnet_cidrs)
+  name         = "subnet-${count.index}"
+  ip_cidr_range = var.subnet_cidrs[count.index]
+  region       = "us-west4"
+  network      = google_compute_network.vpc_network.self_link
+}
+
+# Create Cloud Run service
+resource "google_cloud_run_service" "cloudrun-tf" {
+  name     = var.service_name
+  location = "us-west4"
+
+  template {
+    spec {
+      containers {
+        image = var.service_image
+        ports {
+                    container_port = 80
+                }
+        
+      }
+      
+    }
+  }
+}
