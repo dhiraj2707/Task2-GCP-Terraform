a063f035ed9a1cef44bbb90c55d27cee43fece1b
dhiraj2707 <71545782+dhiraj2707@users.noreply.github.com>
2024-07-13 15:18:26 +0530

    Update outputs.tf

--- bhau/outputs.tf
+++ bhau/outputs.tf
@@ -1,1 +1,9 @@
+output "subnet_ids" {
+  description = "IDs of the created subnets"
+  value       = [for subnet in google_compute_subnetwork.subnet : subnet.id]
+}
 
+output "cloud_run_url" {
+  description = "URL of the deployed Cloud Run service"
+  value       = google_cloud_run_service.cloudrun-tf.status[0].url
+}
