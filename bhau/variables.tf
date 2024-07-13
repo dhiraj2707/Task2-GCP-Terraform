2b2b6723c4d8c88de9d673b652de6387bb5c36eb
dhiraj2707 <71545782+dhiraj2707@users.noreply.github.com>
2024-07-13 15:19:37 +0530

    Update variables.tf

--- bhau/variables.tf
+++ bhau/variables.tf
@@ -1,1 +1,12 @@
+variable "subnet_cidrs" {
+  description = "List of CIDR blocks for subnets"
+  type        = list(string)
+}
 
+variable "service_name" {
+  description = "cloudrun-tf"
+}
+
+variable "service_image" {
+  description = "us-west4-docker.pkg.dev/test-1-429208/test1/my-nginx-image:latest"
+}
